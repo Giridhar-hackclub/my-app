da74878df4e7979091adda6e3281c28cd1ec8adf
Henry Potter <giridhar.talla5139@gmail.com>
2020-10-15 12:58:11 +0000

    created App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
+import Scheme from './Scheme';
 
 function App() {
-  return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
-  );
+    return <Scheme />
 }
 
 export default App;
